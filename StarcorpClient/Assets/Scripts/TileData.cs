4fe575970ac8cfe322492b4cd7b4fde694d2cfcb
Jordan Cottle <jordancottle622@gmail.com>
2020-10-12 21:35:21 -0400

    Update TerrainType to be an enum

--- StarcorpClient/Assets/Scripts/TileData.cs
+++ StarcorpClient/Assets/Scripts/TileData.cs
@@ -3,18 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public enum TerrainType : int
+{
+    Space,
+    Ground,
+    Water,
+    Star
+}
 
+public static class TerrtainTypeExtensions
+{
+    public static int movementCost(this TerrainType t)
+    {
+        switch (t)
+        {
+            case TerrainType.Space:
+                return 2;
+            case TerrainType.Ground:
+                return 5;
+            case TerrainType.Water:
+                return 12;
+            case TerrainType.Star:
+                return 45;
+            default:
+                throw new System.ArgumentException($"Invalid TerrainType {t}");
+        }
+    }
+    public static string name(this TerrainType t)
+    {
+        switch (t)
+        {
+            case TerrainType.Space:
+                return "Space";
+            case TerrainType.Ground:
+                return "Ground";
+            case TerrainType.Water:
+                return "Water";
+            case TerrainType.Star:
+                return "Star";
+            default:
+                throw new System.ArgumentException($"Invalid TerrainType {t}");
+        }
+    }
+}
 [CreateAssetMenu]
 public class TileData : ScriptableObject
 {
     public TileBase[] tiles;
 
-    public int terrainType;
-
-    public static readonly string[] TYPES = { "Space", "Ground", "Water", "Star" };
-
-    public string type
+    public TerrainType terrainType;
+    public string terrainName
     {
-        get => TYPES[this.terrainType];
+        get => terrainType.name();
     }
 }
