e7648342d337888859d21dfdaf7d2fca27bc9fd2
Jordan Cottle <jordancottle622@gmail.com>
2020-10-16 07:56:28 -0400

    Add resource manager to handle emitting gather events

--- StarcorpClient/Assets/Scripts/Resource.cs
+++ StarcorpClient/Assets/Scripts/Resource.cs
@@ -6,4 +6,20 @@ using UnityEngine;
 public class Resource : MonoBehaviour
 {
     public string type;
+
+    public Position position;
+    public string pos; // TODO: Remove when Resources are loaded from server
+    void Start()
+    {
+        this.position = new Position(pos);
+        Debug.Log($"{this.type} at {this.position}");
+
+    }
+
+    public void Initialize()
+    {
+        HexGrid hexGrid = FindObjectOfType<HexGrid>();
+        Vector3 worldPos = hexGrid.getWorldPosition(this.position);
+        this.transform.position = worldPos;
+    }
 }
