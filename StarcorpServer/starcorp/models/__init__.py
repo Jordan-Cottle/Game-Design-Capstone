3632e379c428a76deff17b1c24e5eefea5a7f0ce
Jordan Cottle <jordancottle622@gmail.com>
2020-12-11 23:25:53 -0500

    Add user table

---
+++ StarcorpServer/starcorp/models/__init__.py
@@ -0,0 +1,8 @@
+""" Package for defining all of the database models and their relationships. """
+# pylint: disable=wrong-import-position
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+from .user import User
---
+++ StarcorpServer/starcorp/models/user.py
@@ -0,0 +1,23 @@
+""" Module for defining user related models. """
+
+from sqlalchemy import Column, Integer, String
+
+from models import Base
+
+
+class User(Base):
+    """ Table for tracking the users of the game and their login info. """
+
+    __tablename__ = "User"
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+
+    name = Column(String, nullable=False)
+    salt = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id}, name={self.name}, email={self.email})"
