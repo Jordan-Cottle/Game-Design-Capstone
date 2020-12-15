88edcd75e688883135f11341a2929b7b24162fdd
Jordan Cottle <jordancottle622@gmail.com>
2020-12-15 15:53:06 -0500

    Add get_subclasses utility

--- StarcorpServer/starcorp/utils/__init__.py
+++ StarcorpServer/starcorp/utils/__init__.py
@@ -1,3 +1,11 @@
 """ Package for common utilities. """
 
 from .logging import LOGGER, get_logger
+
+
+def get_subclasses(cls):
+    """ Get all subclasses of a class. """
+
+    for sub_cls in cls.__subclasses__():
+        yield from get_subclasses(sub_cls)
+        yield sub_cls
