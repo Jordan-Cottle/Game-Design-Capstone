bf161502fcfd248689842edd2fc55a8b1e9dddb1
Jordan Cottle <jordancottle622@gmail.com>
2020-12-15 17:24:49 -0500

    Add get ship system database utility

--- StarcorpServer/starcorp/database/ship.py
+++ StarcorpServer/starcorp/database/ship.py
@@ -1,6 +1,6 @@
 """ Module for ship related database utilities. """
 
-from models import ShipChassis
+from models import ShipChassis, ShipSystem
 from database import get_by_name_or_id
 
 
@@ -8,3 +8,9 @@ def get_chassis(session, chassis_id=None, name=None):
     """ Get a ship chassis by it's id or name. """
 
     return get_by_name_or_id(session, ShipChassis, model_id=chassis_id, name=name)
+
+
+def get_ship_system(session, system_id=None, name=None):
+    """ Get a ship sub-system by its id or name. """
+
+    return get_by_name_or_id(session, ShipSystem, model_id=system_id, name=name)
