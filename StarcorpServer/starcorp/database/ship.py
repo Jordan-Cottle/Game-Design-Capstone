d49c1ba2c95c46f4fa959a20cf72c08ac4eb64f0
Jordan Cottle <jordancottle622@gmail.com>
2020-12-15 21:39:14 -0500

    Add create_ship utility

--- StarcorpServer/starcorp/database/ship.py
+++ StarcorpServer/starcorp/database/ship.py
@@ -1,7 +1,7 @@
 """ Module for ship related database utilities. """
 
-from models import ShipChassis, ShipSystem
 from database import get_by_name_or_id
+from models import Ship, ShipChassis, ShipInstalledSystem, ShipSystem
 
 
 def get_chassis(session, chassis_id=None, name=None):
@@ -14,3 +14,20 @@ def get_ship_system(session, system_id=None, name=None):
     """ Get a ship sub-system by its id or name. """
 
     return get_by_name_or_id(session, ShipSystem, model_id=system_id, name=name)
+
+
+def create_ship(session, user, location, chassis, loadout):
+
+    ship = Ship(
+        location_id=location.id, owner_id=user.id, chassis_id=chassis.id, active=True
+    )
+
+    session.add(ship)
+
+    for item in loadout:
+        system = ShipInstalledSystem(system_id=item.id)
+        ship.loadout.append(system)
+
+    session.flush()
+
+    return ship
