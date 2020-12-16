3629ff8b4a76a012159145864b8eb4d7db643b79
Jordan Cottle <jordancottle622@gmail.com>
2020-12-16 16:15:52 -0500

    Add map gen util and some maps

--- StarcorpServer/starcorp/data/enums.py
+++ StarcorpServer/starcorp/data/enums.py
@@ -3,13 +3,20 @@
 from enum import Enum
 
 
-class TileType(int, Enum):
-    """ Enum for types of tiles in the world. """
+class TileType(str, Enum):
+    """Enum for types of tiles in the world.
 
-    SPACE = 0
-    GROUND = 1
-    WATER = 2
-    STAR = 3
+    These types represent types of planetary surfaces.
+    """
+
+    SPACE = " "
+    LUSH = "L"
+    ARID = "D"
+    AQUATIC = "W"
+    SOLAR = "*"
+
+    def __str__(self) -> str:
+        return str(self.value)
 
 
 class Resource(str, Enum):
