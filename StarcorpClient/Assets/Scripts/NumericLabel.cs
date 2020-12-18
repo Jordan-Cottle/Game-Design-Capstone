ec89607106a2d8b9ef7bb82320aef9e3976b31e3
Jordan Cottle <jordancottle622@gmail.com>
2020-12-18 17:19:35 -0500

    Create scripted label label prefab for building ui components

---
+++ StarcorpClient/Assets/Scripts/NumericLabel.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NumericLabel : MonoBehaviour
+{
+
+    [SerializeField]
+    private string _name = "NullAttribute";
+    [SerializeField]
+    private string _valuePrefix = "";
+    [SerializeField]
+    private bool _integerFormat;
+    private float _value = 0;
+
+    public Text NameLabel;
+    public Text ValueLabel;
+
+    public string Name
+    {
+        get => _name; set
+        {
+            _name = value;
+            UpdateLabel();
+        }
+    }
+    public string ValuePrefix
+    {
+        get => _valuePrefix; set
+        {
+            _valuePrefix = value;
+            UpdateLabel();
+        }
+    }
+
+    public int IntValue
+    {
+        get => (int)_value; set
+        {
+            _value = value;
+            _integerFormat = true;
+            UpdateLabel();
+        }
+    }
+
+    public float Value
+    {
+        get => _value; set
+        {
+            _value = value;
+            UpdateLabel();
+        }
+    }
+
+    public bool IntegerFormat
+    {
+        get => _integerFormat; set
+        {
+            _integerFormat = value;
+            UpdateLabel();
+        }
+    }
+
+
+    void Start()
+    {
+        UpdateLabel();
+    }
+
+    void UpdateLabel()
+    {
+        NameLabel.text = Name;
+
+        if (IntegerFormat)
+        {
+            ValueLabel.text = $"{ValuePrefix}{IntValue}";
+        }
+        else
+        {
+            ValueLabel.text = $"{ValuePrefix}{Value:F3}";
+        }
+    }
+}
