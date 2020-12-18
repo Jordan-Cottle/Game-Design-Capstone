5c80d11d7a15940514d139ea83f456edf6e6004b
Jordan Cottle <jordancottle622@gmail.com>
2020-12-18 05:03:09 -0500

    Don't trigger clicks on ui elements

--- StarcorpClient/Assets/Scripts/GameController.cs
+++ StarcorpClient/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 using Newtonsoft.Json.Linq;
 
@@ -123,7 +124,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             StartCoroutine(this.MovePlayerTo(Input.mousePosition));
         }
