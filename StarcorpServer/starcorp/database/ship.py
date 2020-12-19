430625173df4b1586de95a8d1e8769a4bbe165db
Jordan Cottle <jordancottle622@gmail.com>
2020-12-18 23:12:22 -0500

    Move get systems utility

--- StarcorpServer/starcorp/database/ship.py
+++ StarcorpServer/starcorp/database/ship.py
@@ -1,7 +1,10 @@
 """ Module for ship related database utilities. """
 
+from collections import Counter
+
 from sqlalchemy.orm.exc import NoResultFound
 
+from data import ShipSystemAttributeType
 from database import get_by_name_or_id, get_location
 from models import (
     ResourceType,
@@ -101,3 +104,42 @@ def get_upgrade(session, ship_system):
 
     LOGGER.debug(f"No upgrade found for {ship_system}")
     return None
+
+
+def get_systems(session, ship):
+    """ Get all the systems, and the next upgrades, for a ship. """
+
+    attribute_totals = Counter()
+    systems = []
+    for slot in ship.loadout:
+
+        upgrade = get_upgrade(session, slot.system)
+        if upgrade is not None:
+            upgrade_cost = upgrade.get_attribute(ShipSystemAttributeType.BASE_COST)
+        else:
+            upgrade_cost = -1
+
+        data = {
+            "name": slot.system.name,
+            "upgrade_cost": upgrade_cost,
+        }
+        attributes = []
+        for attribute in slot.system.attributes:
+            if upgrade is not None:
+                upgraded_value = upgrade.get_attribute(attribute.type)
+            else:
+                upgraded_value = -1
+
+            attr_data = {
+                "name": attribute.type,
+                "value": attribute.value,
+                "upgraded_value": upgraded_value,
+            }
+            attribute_totals[attribute.type] += attribute.value
+            attributes.append(attr_data)
+
+        data["attributes"] = attributes
+        data.update(attribute_totals)
+        systems.append(data)
+
+    return systems
